2a4c6577310dd4d5aa02cef403eb67104f81e337
Thadeu <marcio.guimaraes@maistodos.com.br>
2026-01-02 12:25:04 -0300

    chore(helm): disable helm provider and metrics charts
    Temporarily commenting out helm resources and provider to deploy vanilla EKS.

--- providers.tf
+++ providers.tf
@@ -8,10 +8,10 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.default.token
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.default.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.main.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.default.token
+#   }
+# }
